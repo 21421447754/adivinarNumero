95aac7f09de635e817b6bc6d2acba796a0f549b8
miguel <martinezalfonso583@gmail.com>
2022-05-30 11:29:11 -0500

    Initial commit 1

---
+++ app/src/main/java/martinez/miguel/adivinarnumero/ponerNumeros.kt
@@ -0,0 +1,11 @@
+package martinez.miguel.adivinarnumero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ponerNumeros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poner_numeros)
+    }
+}
